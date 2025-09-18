ad60e04e7486a75dc7a3618d3dcc75f7d1341640
jlbuild <juliabuildbot@gmail.com>
2025-09-18 19:18:05 +0000

    aws_c_s3_jll build 0.9.0+0

--- src/wrappers/riscv64-linux-gnu.jl
+++ src/wrappers/riscv64-linux-gnu.jl
@@ -4,10 +4,12 @@ export libaws_c_s3
 using aws_checksums_jll
 using aws_c_http_jll
 using aws_c_auth_jll
+using aws_c_common_jll
+using s2n_tls_jll
 JLLWrappers.@generate_wrapper_header("aws_c_s3")
 JLLWrappers.@declare_library_product(libaws_c_s3, "libaws-c-s3.so.0unstable")
 function __init__()
-    JLLWrappers.@generate_init_header(aws_checksums_jll, aws_c_http_jll, aws_c_auth_jll)
+    JLLWrappers.@generate_init_header(aws_checksums_jll, aws_c_http_jll, aws_c_auth_jll, aws_c_common_jll, s2n_tls_jll)
     JLLWrappers.@init_library_product(
         libaws_c_s3,
         "lib/libaws-c-s3.so",
